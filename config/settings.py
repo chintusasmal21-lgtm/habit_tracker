cdee2db260cab9b2a60952ad15a5986d131f0fce
Chintu Sasmal <chintu.sasmal21@gmail.com>
2026-07-27 13:55:10 +0530

    Configure Brevo SMTP email

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
-RESEND_API_KEY = os.environ.get("RESEND_API_KEY")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,19 +139,34 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Email Configuration - Brevo SMTP
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = os.environ.get(
+    "EMAIL_HOST",
+    "smtp-relay.brevo.com"
+)
 
-EMAIL_PORT = 587
+EMAIL_PORT = int(
+    os.environ.get("EMAIL_PORT", 587)
+)
 
 EMAIL_USE_TLS = True
 
+# Brevo SMTP Login
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+
+# Brevo SMTP Key
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
 EMAIL_TIMEOUT = 10
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# Sender email address
+DEFAULT_FROM_EMAIL = os.environ.get(
+    "DEFAULT_FROM_EMAIL",
+    EMAIL_HOST_USER
+)
 
 
 LOGIN_REDIRECT_URL = '/dashboard/'
